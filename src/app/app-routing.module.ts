1b6745a6c8cf41f87aa8d95f29d65a98f12269e7
Georgi Kolishovski <georgi.kolishovski@jax.org>
2019-02-04 08:49:17 -0500

    created components for the different 'Docs' menu sun items and populated content for some of them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { DocsComponent } from './docs/docs.component';
+
+import { DocsTutorialComponent } from './docs/docs-tutorial.component';
+import { DocsClientAPIComponent } from './docs/docs-client-api.component';
+import { DocsServiceAPIComponent } from './docs/docs-python-api.component';
+import { DocsDataPrepComponent } from './docs/docs-data-prep.component';
+import { DocsFeaturesComponent } from './docs/docs-features.component';
+import { DocsDockerComponent } from './docs/docs-docker.component';
+import { DocsConfigsComponent } from './docs/docs-configs.component';
+import { DocsContributorsComponent } from './docs/docs-contributors.component';
+
 import { ExamplesComponent } from './examples/examples.component';
 import { HomeComponent } from './home/home.component';
 
@@ -10,6 +20,14 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'docs', component: DocsComponent },
+  { path: 'docs-tutorial', component: DocsTutorialComponent },
+  { path: 'docs-client-api', component: DocsClientAPIComponent },
+  { path: 'docs-service-api', component: DocsServiceAPIComponent },
+  { path: 'docs-data-prep', component: DocsDataPrepComponent },
+  { path: 'docs-browser-features', component: DocsFeaturesComponent },
+  { path: 'docs-docker', component: DocsDockerComponent },
+  { path: 'docs-configs', component: DocsConfigsComponent },
+  { path: 'docs-contributors', component: DocsContributorsComponent },
   { path: 'examples', component: ExamplesComponent }
 ];
 
---
+++ src/app/docs/docs-menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sb-docs-menu',
+  template: `
+    <section class="sidenav-content">
+      <a class="nav-link" [routerLink]="['/docs-tutorial']">Tutorial</a>
+      <a class="nav-link" [routerLink]="['/docs-client-api']">Client API</a>
+      <a class="nav-link" [routerLink]="['/docs-service-api']">Python Service API</a>
+      <a class="nav-link" [routerLink]="['/docs-data-prep']">Data Sources and Formats</a>
+      <a class="nav-link" [routerLink]="['/docs-browser-features']">Browser Features</a>
+      <a class="nav-link" [routerLink]="['/docs-docker']">Docker</a>
+      <a class="nav-link" [routerLink]="['/docs-configs']">View Configs</a>
+      <a class="nav-link" [routerLink]="['/docs-contributors']">Contributor Guidelines</a>
+    </section>
+  `,
+  styleUrls: ['./docs.component.scss']  
+})
+
+export class DocsMenuComponent {
+	
+}
