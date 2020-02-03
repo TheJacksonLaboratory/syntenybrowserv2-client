8260cbf2f56dfe4b8ba79b58c24a6a60d9f07a84
Anna Lamoureux <anna.lamoureux@jax.org>
2020-02-03 11:45:30 -0500

    Future simplification of the handling of routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // all of the pages the user can navigate to with their route names
-  pages = {
-    browser: ['/browser'],
-    about: ['/about'],
-    examples: ['/examples'],
-    docs: ['/docs'],
-  };
-
-  /**
-   * Returns the list of page names
-   */
-  getPages(): string[] {
-    return Object.keys(this.pages);
-  }
+  // all of the pages the user can navigate to
+  pages = ['browser', 'about', 'examples', 'docs'];
 }
