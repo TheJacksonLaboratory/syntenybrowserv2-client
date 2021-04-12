d6d0a861b287cde30a6a29ddb7797eb8f07edf26
Georgi Kolishovski <georgi.kolishovski@jax.org>
2021-04-12 13:49:12 -0400

    added environment variable(s) for outside repos and applied to the 'Data Sources/Formats' template/view

--- src/app/docs/sections/docs-data-prep/docs-data-prep.component.ts
+++ src/app/docs/sections/docs-data-prep/docs-data-prep.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'data-prep',
   templateUrl: './docs-data-prep.component.html',
   styleUrls: ['./../../docs.component.scss'],
 })
-export class DocsDataPrepComponent {}
+export class DocsDataPrepComponent {
+  private dataUrl: string = environment.sourceData;
+  // console.log(dataUrl);
+}
